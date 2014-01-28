30122288d139056aaffa7049fa63b010de25cefa
John Olmsted <null>
2014-01-28 11:30:35 -0800

    Add categories.erb and categories routing to index.rb

--- source/craigslist/app/controllers/index.rb
+++ source/craigslist/app/controllers/index.rb
@@ -1,4 +1,14 @@
 get '/' do
-  # Look in app/views/index.erb
+  @categories = Category.all
   erb :index
+end
+
+get '/categories/:id' do
+  @posts = Post.joins(:category).where(categories: {id: params[:id]})
+  erb :categories
+end
+
+get '/posts/:id' do
+  @post = Post.find(params[:id])
+  erb :posts
 end
