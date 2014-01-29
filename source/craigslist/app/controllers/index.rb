afa888ff4f007f2f724a2e63ec8b25a0c841f106
Emily Foley & Roy Lee <pair+EmFoley.lee-sh-roy@devbootcamp.com>
2014-01-28 23:03:38 -0800

    Continued WIP

--- source/craigslist/app/controllers/index.rb
+++ source/craigslist/app/controllers/index.rb
@@ -6,40 +6,41 @@ get '/' do
   erb :index
 end
 
-get '/*' do
-  @category_name = params[:splat][0].capitalize
-  @category = Category.where(name: @category_name).first
-  @posts = find_posts
-  erb @category_name.to_sym
-end
-
 get '/newpost' do
   erb :newpost
 end
 
-get '/:category/:post' do
-  @category_name = params[:splat][0].capitalize
-  @category = Category.where(name: params[:name])[0]
-  @posts = Post.where(title: params[:title])[0]
-  erb :viewpost
-end
-
-post '/confirmation' do
-  @posts = Post.create(title: params[:title], content: params[:content], price: params[:price].to_f, email: params[:email], category_id: params[:category_id])
-  @category_id = params[:category_id]
-  @category = Category.where(id: params[:category_id])[0]
-  @secret_part = rand(100000)
-  @url = "localhost:9393/#{@category.name}/#{@posts.title}/#{@secret_part}"
-  erb :confirmation
-end
-
-get '/category=:category/:title/:secret' do
-  @post = Post.where(title: params[:title])[0]
+get '/category=:category/:post_id/:secret' do
+  @post = Post.where(id: params[:post_id].to_i)[0]
   if @post.secret.to_i == params[:secret].to_i
     erb :master_edit
   else
     erb :wrong
   end
+end
+
+get '/:category/:post_id' do
+# get '/:category/:post' do
+  @category = Category.where(name: params[:name])[0]
+  @posts = Post.where(id: params[:post_id])[0]
+  erb :viewpost
+end
+
+post '/confirmation' do
+  Post.create(title: params[:title], content: params[:content], price: params[:price].to_f, email: params[:email], category_id: params[:category_id])
+  @category_id = params[:category_id]
+  @category = Category.where(id: params[:category_id])[0]
+  @secret_part = rand(100000)
+  @post_id = Post.all.length
+  @url = "localhost:9393/#{@category.name}/#{@posts_id}/#{@secret_part}"
+  erb :confirmation
+end
+
+get '/*' do
+  @category_name = params[:splat][0].capitalize
+  @category = Category.where(name: @category_name).first
+  @posts = find_posts
+  erb @category_name.to_sym
 end
 
 #  Post.create(title: params[:title], content: params[:content], price: params[:price].to_f, email: params[:email], category_id: params[:category_id])
