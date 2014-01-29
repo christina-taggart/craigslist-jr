d8dbf0f597fc670e0b3db050d48df01e9a47cc66
Apprentice <apprentice@dbc15.local>
2014-01-28 23:04:44 -0800

    still working on release 0

--- source/craigslist/app/controllers/index.rb
+++ source/craigslist/app/controllers/index.rb
@@ -1,14 +1,33 @@
-
-
 get '/' do
-  # Look in app/views/index.erb
-  @posts = Post.all
+  @categories = Category.pluck(:name)
   erb :index
+end
+
+get '/display-posts/:category_name' do
+  category = Category.where(name: params[:category_name]).first
+  @category_posts = category.posts
+  erb :display_posts
+end
+
+get '/create-categories' do
+  erb :create_categories
+end
+
+get '/create-posts' do
+  erb :create_posts
 end
 
 post '/posts' do
   p params
   Post.create(params)
+  redirect '/'
+end
 
+post '/categories' do
+  Category.create(params)
+  redirect '/'
+end
+
+get '/display-item/:item' do
   redirect '/'
 end
--- source/craigslist/app/models/post.rb
+++ source/craigslist/app/models/post.rb
@@ -1,10 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :category
 
-
-  # def self.posts(post_info = {})
-  #   description = post_info[:description]
-  #   price = post_info[:price]
-  #   email = post_info[:email]
-  # end
+  validate_associated :category
 end
