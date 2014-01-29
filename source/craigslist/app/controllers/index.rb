9d8fcff813b059267cfa1c298b9420d8eb1b97a5
Christina Taggart & Ricardo A. Rubio <pair+christina-taggart.rickarubio@devbootcamp.com>
2014-01-28 19:09:47 -0800

    Added updated post page, and link from successful post page to page of postings

--- source/craigslist/app/controllers/index.rb
+++ source/craigslist/app/controllers/index.rb
@@ -12,8 +12,6 @@ end
 get '/posts/:id' do
   @current_post_id = params[:id]
   @post = Post.find(@current_post_id)
-  puts "you are here"
-  p params
   erb :posts
 end
 
@@ -23,21 +21,25 @@ get '/posts' do
 end
 
 post '/post_success' do
-  puts "you got to the post part"
   @edit_url = generate_secret_url
-  #create_new_post(params)
+  create_new_post(params)
+  @post_id = Post.where(secret_url: @edit_url)[0].id.to_s
   erb :post_success
 end
 
 # this will become the route to the
 # "secret" edit url for the post
 get '/edit/:edit_url' do
-
+  redirect 'https://github.com/doesntexist' unless is_valid_secret_url?(params[:edit_url])
+  @categories = Category.all
+  @post = Post.where(secret_url: params[:edit_url])[0]
+erb :edit_post
 end
 
 
+
 def create_new_post(params)
-  Post.create(
+  Post.create!(
     title: params["title"],
     price: params["price"],
     description: params["description"],
@@ -45,8 +47,13 @@ def create_new_post(params)
     state: params["state"],
     email: params["email"],
     secret_url: @edit_url,
-    category_id: Category.where(title: params["category"]).id
+    category_id: Category.where(title: params["category"])[0].id
     )
+end
+
+def is_valid_secret_url?(secret_url)
+  valid = Post.where(secret_url: secret_url)
+  valid.empty? ? false : true
 end
 
 def categories_info
